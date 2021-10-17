15e2fcb2e0cb32e50c799a22ffc41957f9c17465
Bruno <Bruno@DESKTOP-GALUFR4>
2021-10-17 10:40:26 -0300

    refatoração da main

--- TrabalhoFinalParte3/src/cosmetic/Cosmetic.java
+++ TrabalhoFinalParte3/src/cosmetic/Cosmetic.java
@@ -1,12 +1,20 @@
 package cosmetic;
 
+import cosmetic.ui.CosmeticInterface;
 import cosmetic.ui.impl.CosmeticTextInterface;
 
 public class Cosmetic {
 
+	CosmeticInterface cosmeticInterface;
+	
 	public static void main(String[] args) {
-		CosmeticTextInterface ct = new CosmeticTextInterface();
-		ct.createAndShowUI();
+		Cosmetic cosmetic = new Cosmetic();
+		
+		cosmetic.cosmeticInterface.createAndShowUI();
+	}
+	
+	public Cosmetic() {
+		cosmeticInterface = new CosmeticTextInterface();
 	}
 
 }
